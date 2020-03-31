b4a972b9c921339a873c8f6eb0a619cc820433ad
ilam56 <ilam56@mail.missouri.edu>
2020-03-31 13:04:14 -0500

    most likely final commit

--- assignments/assignment-seven/project.js
+++ assignments/assignment-seven/project.js
@@ -68,6 +68,9 @@ function setRepo() {
 function filldata(){
     fillTopCommiters();
     fillSummary();
+    fillIssues();
+    console.log("repo group: " + repoGroup);
+    console.log("repo: " + repo);
 }
 
 function show(targetID){
@@ -85,15 +88,19 @@ function fillTopCommiters(){
             
             var dataArray = [['Commiters', 'Commits']];
             var i=0;
-            for(i=0;i<obj.length;i++){
-                dataArray.push([obj[i].email,obj[i].commits]);
+            if(obj[0]!== undefined){
+                for(i=0;i<obj.length;i++){
+                    dataArray.push([obj[i].email,obj[i].commits]);
+                }
             }
-            console.log(repoGroup);
             var data = google.visualization.arrayToDataTable(dataArray);
 
             var options = {'title':'Top Commiters', 'width':550, 'height':400};
             var chart = new google.visualization.PieChart(document.getElementById('commitpiechart'));
             chart.draw(data, options);
+        }
+        else if(this.readyState == 4){
+            document.getElementById('commitpiechart').innerHTML = '<h1>Internal Error</h1>';
         }
     };
     if(repo != "NULL"){
@@ -105,21 +112,68 @@ function fillTopCommiters(){
     
 }
 
+function fillIssues(){
+    document.getElementById('issuep').innerHTML = "";
+    if(repo != "NULL"){
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                var response = this.responseText;
+                var obj = JSON.parse(response);
+                var date = "";
+                var year = "";
+                var dataArray = [['Date', 'Issues']];
+                var i=0;
+                if(obj[0] !== undefined){
+                    for(i=0;i<obj.length;i++){
+                        date = obj[i].date;
+                        year = date.substring(0,7);
+                        dataArray.push([year, obj[i].issues]);
+                    }
+                }
+                if(obj[0] !== undefined){
+                    var data = google.visualization.arrayToDataTable(dataArray);
+
+                    var options = {'title':'Issues', 'width':700, 'height':400};
+                    var chart = new google.visualization.LineChart(document.getElementById('issuechart'));
+                    chart.draw(data, options);
+                    var curIssue = obj[obj.length - 1].issues;
+                    document.getElementById('issuep').innerHTML = "Issses Currently Active: " + curIssue;
+                }
+                else {
+                    document.getElementById('issuechart').innerHTML = "No Data Available";
+                    document.getElementById('issuep').innerHTML = "Issses Active: No Data Available";
+                }
+            }
+            else if(this.readyState == 4){
+                document.getElementById('issuechart').innerHTML = '<h1>Internal Error</h1>';
+            }
+        };
+        if(repo != "NULL"){
+            xhttp.open("GET", "http://czi2.osshealth.io:5153/api/unstable/repos/"+repo+ '/issues-active?period=month', true);
+        } else {
+            xhttp.open("GET", "http://czi2.osshealth.io:5153/api/unstable/repo-groups/"+repoGroup+"/issues-active?period=month", true);
+        }
+        xhttp.send();  
+    } else {
+        document.getElementById('issuechart').innerHTML = '<h1>Please Select a Repo</h1>';
+    }
+}
+
 function fillSummary(){
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             var text = this.responseText;
             var obj = JSON.parse(text);
-            var i =0;
-            var summaryText = "";
-            summaryText += 'Watcher Count: ' + obj[0].watcher_count + '<br>';
-            summaryText += 'Stars Count: ' + obj[0].stars_count + '<br>';
-            summaryText += 'Fork Count: ' + obj[0].fork_count + '<br>';
-            summaryText += 'Merged Count: ' + obj[0].merged_count + '<br>';
-            summaryText += 'Commiter Count: ' + obj[0].committer_count + '<br>';
-            summaryText += 'Commit Count: ' + obj[0].commit_count + '<br>';
-            document.getElementById("summaryInfo").innerHTML = summaryText;
+            
+            document.getElementById('watcher').innerHTML = obj[0].watcher_count;
+            document.getElementById('stars').innerHTML = obj[0].stars_count;
+            document.getElementById('fork').innerHTML = obj[0].fork_count;
+            document.getElementById('merged').innerHTML = obj[0].merged_count;
+            document.getElementById('committer').innerHTML = obj[0].committer_count;
+            document.getElementById('commit').innerHTML = obj[0].commit_count;
+            
         }
     };
     if(repo != "NULL"){
