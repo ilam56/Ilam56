481b2ac9cbd4240d06c93e09893f7ac812cf3b4f
ilam56 <ilam56@mail.missouri.edu>
2020-05-17 23:37:44 -0500

    update to sprint project

---
+++ assignments/sprint-six/my-google/user.py
@@ -0,0 +1,58 @@
+from flask_login import UserMixin
+
+from db import get_db
+
+
+class User(UserMixin):
+    def __init__(self, id_, name, email, profile_pic):
+        self.id = id_
+        self.name = name
+        self.email = email
+        self.profile_pic = profile_pic
+
+    @staticmethod
+    def get(user_id):
+        db = get_db()
+        user = db.execute(
+            "SELECT * FROM user WHERE id = ?", (user_id,)
+        ).fetchone()
+        if not user:
+            return None
+
+        user = User(
+            id_=user[0], name=user[1], email=user[2], profile_pic=user[3]
+        )
+        return user
+
+    @staticmethod
+    def create(id_, name, email, profile_pic):
+        db = get_db()
+        db.execute(
+            "INSERT INTO user (id, name, email, profile_pic)"
+            " VALUES (?, ?, ?, ?)",
+            (id_, name, email, profile_pic),
+        )
+        db.commit()
+
+    @staticmethod
+    def setQuestions(id_, quest1, quest2, quest3):
+        db = get_db()
+        db.execute(
+            "UPDATE user SET quest1 = ?, quest2 = ?, quest3 = ? WHERE id = ?",
+            (quest1, quest2, quest3, id_),
+        )
+        db.commit()
+    
+    @staticmethod
+    def getQuestion(user_id, quest):
+        return_val = ""
+        db = get_db()
+        question = db.execute(
+            "SELECT ? FROM user WHERE id = ?", (quest, user_id,)
+        ).fetchone()
+        print(question)
+        if not question:
+            return None
+        for x in question:
+            return_val = x
+        return return_val
